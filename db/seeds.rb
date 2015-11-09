05623d902d2ac0d8b60a91aded34971f7bc73902
SCOTT WALSH <SCMWALSH@GMAIL.COM>
2015-11-09 15:22:27 -0500

    Making each picture show at its own URL with dynamic routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 Picture.create!(
-	:title => "At the camel fair in Pushkar"
-	:artist => "Scott Walsh"
+	:title => "At the camel fair in Pushkar",
+	:artist => "Scott Walsh",
 	:url => "http://static1.squarespace.com/static/54a854d9e4b0f7d742e0cf8e/54aeed5ae4b0aa2915c2034a/54aeeeb3e4b0eeb82c64a0dd/1420750525639/asia2329.jpg?format=750w"
 )
 
 Picture.create!(
-	:title => "The brides first look"
-	:artist => "Scott Walsh"
+	:title => "The brides first look",
+	:artist => "Scott Walsh",
 	:url => "http://static1.squarespace.com/static/54a854d9e4b0f7d742e0cf8e/561ac563e4b0906d17d058a9/5627e6d5e4b07819aa6d73e1/1445455585835/ML_139.jpg?format=750w"
 )
 
 Picture.create!(
-	:title => "For the love of music"
-	:artist => "Scott Walsh"
+	:title => "For the love of music",
+	:artist => "Scott Walsh",
 	:url => "http://static1.squarespace.com/static/54a854d9e4b0f7d742e0cf8e/55d66abce4b054689cac975b/55d66be7e4b0c9560c49034e/1440115704602/ALT_3.jpg?format=750w"
 )
 
